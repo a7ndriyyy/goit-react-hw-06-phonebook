a9ca9d59f0acb792d42b981d6cfa8b39bc607f4d
a7ndriyyy <andrii.nastych@gmail.com>
2023-10-21 11:50:01 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,28 +4,29 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const contactsSlice = createSlice({
-    name: 'contacts',
-    initialState: {
-        contacts: [],
-        filter: '',
-    }, reducers: {
-        addContact: (state, action) => {
-            state.contacts.push(action.payload);
-        }, removeContact: (state, action) => {
-            state.contacts = state.contacts.filter(
-                contact => contact.id !== action.payload.id
-            );
-        }, setFilter: (state, action) => {
-            state.filter = action.payload;
-        },
+  name: 'contacts',
+  initialState: {
+    contacts: [],
+    filter: '',
+  },
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
     },
+    removeContact: (state, action) => {
+      state.contacts = state.contacts.filter(el => el.id !== action.payload);
+    },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
 });
 export const { addContact, removeContact, setFilter } = contactsSlice.actions;
 
 const persistConfig = {
-    key: 'contacts',
-    storage,
-    whitelist: ['contacts'],
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
 };
 export const persistedReducer = persistReducer(
   persistConfig,
